dab42543a36785efffa27a49ba5a93984b34bdb9
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-08-08 21:28:30 +0300

    Add public assets; update .gitignore

--- app/ui/config/__init__.py
+++ app/ui/config/__init__.py
@@ -4,6 +4,7 @@ from typing import Final
 import streamlit as st
 from streamlit_cookies_controller import CookieController
 
+
 FAST_API_BASE_URL: Final[str] = st.secrets.get('FAST_API_BASE_URL')
 
 FAST_API_CONNECT_TIMEOUT: Final[int] = st.secrets.get(
